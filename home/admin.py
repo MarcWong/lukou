d51f22037d37eda0c1104f2657c6bbec7b7cce3e
jiangbowen <yezi@offpay.cn>
2019-07-03 14:32:43 +0800

    主页面

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import News
 # Register your models here.
+# admin.site.register(News)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+
+    '''设置列表可显示的字段'''
+    list_display = ['title', 'url', 'create_time']
+
+    '''每页显示条目数'''
+    list_per_page = 20
+
+    '''按发布日期排序'''
+    ordering = ('-create_time',)
--- home/urls.py
+++ home/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^', views.index, name='index'),
 ]
