9c6eaeca6806cb54b09963516fb93e577b896201
jiangbowen <yezi@offpay.cn>
2019-07-24 23:46:33 +0800

    增加分页

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, JsonResponse
 from .models import News,Tdk
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import markdown
 # Create your views here.
 
@@ -17,33 +18,35 @@ def index(request):
 @csrf_exempt
 def news(request):
 
-    try:
-        curpage = int(request.POST.get('pageIndex', '1'))
-        pagesize = int(request.POST.get('pageSize', '5'))
-        year = request.POST.get('year','2019')
+    year = request.GET.get('years','2019')
 
-    except ValueError:
-        curpage = 1
+    news_data = News.objects.filter(time__year=year).all().order_by('-update_time')
+    paginator = Paginator(news_data, 2)
+    page_num = request.GET.get('page', 1)  # 获取url的页码参数。GET返回字典，page_num默认为1
+    page_of_blogs = paginator.get_page(page_num)
+    print(page_of_blogs)
+    current_page_num = page_of_blogs.number  # 获取当前页码
+    # 获取前后各页
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + list(
+        range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
+    # 加上省略号
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+    #  加上首尾页码
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
 
-    startPos = (curpage - 1) * pagesize
-    endPos = startPos + pagesize
+    context = {}
+    context['res'] = page_of_blogs.object_list
+    context['page_of_blogs'] = page_of_blogs
+    context['page_range'] = page_range
 
-    news_data = News.objects.filter(time__year=year).all()[startPos:endPos]
-    print(news_data)
-    json_list = []
-    for item in news_data:
-        print(item)
-        json_dict = {}
-        json_dict["title"] = item.title
-        json_dict["id"] = item.id
-        json_dict["digest"] = item.digest
-        json_dict["img"] = str(item.img)
-        json_dict["url"] = item.url
-        json_dict["time"] = str(item.time.strftime("%Y-%m-%d"))
-        json_list.append(json_dict)
-    # print(json_list)
-    return render(request, 'news.html', {"res":json_list})
-    # return HttpResponse(json.dumps(json_list), content_type="application/json")
+    return render(request, 'news.html', context)
+    # return render(request, 'news.html', {"res":json_list,'page_range': page_range})
 
 def new_markdown(request):
     id = request.GET['id']
